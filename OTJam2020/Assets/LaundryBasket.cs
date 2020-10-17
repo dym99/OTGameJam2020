91fcfbd374aa8cd2191d7054d30eef782d982ab6
Dylan Moore <dylan.moore@insanitycorr.com>
2020-10-17 17:59:30 -0400

    Level 2

---
+++ OTJam2020/Assets/LaundryBasket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaundryBasket : MonoBehaviour
+{
+    public Animator m_animator;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Player player = null;
+        if (other.gameObject.TryGetComponent<Player>(out player)) {
+            Destroy(other.gameObject);
+            m_animator.SetTrigger("Kerchunk");
+        }
+    }
+}
