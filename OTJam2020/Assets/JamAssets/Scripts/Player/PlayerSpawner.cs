ea1964a379ba6a9d0cc62eb622d5d33a672da35c
Dylan Moore <dylan.moore@insanitycorr.com>
2020-10-17 15:25:18 -0400

    Washing Machine

---
+++ OTJam2020/Assets/JamAssets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    public Transform m_spawnPoint;
+    public GameObject m_playerPrefab;
+
+    public void SpawnPlayer() {
+        GameObject go = Instantiate(m_playerPrefab);
+        go.transform.position = m_spawnPoint.position;
+    }
+}
