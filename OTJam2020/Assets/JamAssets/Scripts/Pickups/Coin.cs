b233d4b8dd9cc4f69d9d6c717fa03e401e53fdf3
Dylan Moore <dylan.moore@insanitycorr.com>
2020-10-17 19:18:32 -0400

    Update level 2

---
+++ OTJam2020/Assets/JamAssets/Scripts/Pickups/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject m_coinEffect;
+    public int m_score = 1000;
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Player player = null;
+        if (other.TryGetComponent<Player>(out player)) {
+            GameObject go = Instantiate(m_coinEffect);
+            go.transform.position = this.transform.position;
+            Destroy(go, 1.0f);
+            Destroy(this.gameObject);
+        }
+    }
+}
